d13a0825e1acdb02a29e0c98866b869a920096f9
glyceryl <494192175@qq.com>
2024-08-03 09:20:41 +0800

    Add a new creative mode tab.

--- src/main/java/com/glyceryl6/staff/Main.java
+++ src/main/java/com/glyceryl6/staff/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static final String MOD_ID = "staff_of_the_king_orange";
 
     public Main(IEventBus modEventBus, ModContainer modContainer) {
+        KOCreativeModeTabs.CREATIVE_MODE_TABS.register(modEventBus);
         KOBlockEntityTypes.BLOCK_ENTITY_TYPES.register(modEventBus);
         KODataComponents.DATA_COMPONENT_TYPE.register(modEventBus);
         KOParticleTypes.PARTICLE_TYPES.register(modEventBus);
