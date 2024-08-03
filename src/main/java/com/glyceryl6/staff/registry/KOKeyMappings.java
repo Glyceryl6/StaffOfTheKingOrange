2c602f14a5e0286e3c6911e7b4bb4607f69d5b8c
glyceryl <494192175@qq.com>
2024-08-03 09:15:28 +0800

    Add a new category for my Key Mapping.

--- src/main/java/com/glyceryl6/staff/registry/KOKeyMappings.java
+++ src/main/java/com/glyceryl6/staff/registry/KOKeyMappings.java
@@ -1,15 +1,18 @@
 package com.glyceryl6.staff.registry;
 
+import com.glyceryl6.staff.Main;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 
 public class KOKeyMappings {
 
+    public static final String CATEGORY_STAFF = "key.categories." + Main.MOD_ID;
+
     public static final KeyMapping ADD_REMOVE_KEYBINDING = new KeyMapping(
-            "key.staff.add_remove_block", InputConstants.KEY_R, KeyMapping.CATEGORY_MISC);
+            "key.staff.add_remove_block", InputConstants.KEY_R, CATEGORY_STAFF);
     public static final KeyMapping RANDOM_CHANGE_KEYBINDING = new KeyMapping(
-            "key.staff.random_change_block", InputConstants.KEY_B, KeyMapping.CATEGORY_MISC);
+            "key.staff.random_change_block", InputConstants.KEY_B, CATEGORY_STAFF);
     public static final KeyMapping CONTINUOUS_MODE_KEYBINDING = new KeyMapping(
-            "key.staff.continuous_mode", InputConstants.KEY_C, KeyMapping.CATEGORY_MISC);
+            "key.staff.continuous_mode", InputConstants.KEY_C, CATEGORY_STAFF);
 
 }
