902e37d33217a061dfc0abce2d3eaf1092cc757c
glyceryl <494192175@qq.com>
2024-08-12 02:30:40 +0800

    Optimize registry codes.

--- src/main/java/com/glyceryl6/staff/registry/KOCreativeModeTabs.java
+++ src/main/java/com/glyceryl6/staff/registry/KOCreativeModeTabs.java
@@ -1,23 +1,17 @@
 package com.glyceryl6.staff.registry;
 
 import com.glyceryl6.staff.Main;
-import com.glyceryl6.staff.utils.StaffUniversalUtils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class KOCreativeModeTabs {
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Main.MOD_ID);
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NORMAL = CREATIVE_MODE_TABS.register("normal", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup." + Main.MOD_ID))
-            .icon(() -> {
-                ItemStack stack = new ItemStack(KOItems.STAFF);
-                StaffUniversalUtils.setDefaultComponent(stack);
-                return stack;
-            }).displayItems((parameters, output) -> KOItems.ITEMS.getEntries().forEach(holder -> output.accept(holder.get()))).build());
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NORMAL = CREATIVE_MODE_TABS.register("normal", () -> CreativeModeTab
+            .builder().title(Component.translatable("itemGroup." + Main.MOD_ID)).icon(() -> KOItems.STAFF.get().getDefaultInstance())
+            .displayItems((parameters, output) -> KOItems.ITEMS.getEntries().forEach(holder -> output.accept(holder.get()))).build());
 
 }
--- src/main/java/com/glyceryl6/staff/registry/KODataComponents.java
+++ src/main/java/com/glyceryl6/staff/registry/KODataComponents.java
@@ -2,8 +2,10 @@ package com.glyceryl6.staff.registry;
 
 import com.glyceryl6.staff.Main;
 import com.glyceryl6.staff.component.Staffs;
+import com.mojang.serialization.Codec;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.world.item.component.CustomData;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -12,11 +14,18 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class KODataComponents {
 
     public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPE = DeferredRegister.create(Registries.DATA_COMPONENT_TYPE, Main.MOD_ID);
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<CustomData>> STAFF_CORE_STATE = DATA_COMPONENT_TYPE.register("staff_core_state",
-            () -> DataComponentType.<CustomData>builder().persistent(CustomData.CODEC).networkSynchronized(CustomData.STREAM_CODEC).cacheEncoding().build());
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<CustomData>> STAFF_COMMAND = DATA_COMPONENT_TYPE.register("staff_command",
-            () -> DataComponentType.<CustomData>builder().persistent(CustomData.CODEC).networkSynchronized(CustomData.STREAM_CODEC).cacheEncoding().build());
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<CustomData>> STAFF_CORE_STATE = registerCustomData("staff_core_state");
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<CustomData>> STAFF_BINDING_COMMAND = registerCustomData("staff_binding_command");
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<CustomData>> STAFF_SURROUNDING_BLOCK = registerCustomData("staff_surrounding_block");
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<Staffs>> STAFFS = DATA_COMPONENT_TYPE.register("staffs",
             () -> DataComponentType.<Staffs>builder().persistent(Staffs.CODEC).networkSynchronized(Staffs.STREAM_CODEC).cacheEncoding().build());
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Long>> RANDOM_SEED = DATA_COMPONENT_TYPE.register("random_seed",
+            () -> DataComponentType.<Long>builder().persistent(Codec.LONG).networkSynchronized(ByteBufCodecs.VAR_LONG).build());
+
+    private static DeferredHolder<DataComponentType<?>, DataComponentType<CustomData>> registerCustomData(String name) {
+        return DATA_COMPONENT_TYPE.register(name, () ->
+                DataComponentType.<CustomData>builder().persistent(CustomData.CODEC)
+                .networkSynchronized(CustomData.STREAM_CODEC).cacheEncoding().build());
+    }
 
 }
