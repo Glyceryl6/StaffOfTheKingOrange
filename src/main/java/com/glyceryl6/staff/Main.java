92d88682862ca50c9563f026196b46f8ffebf9ed
glyceryl <494192175@qq.com>
2024-09-14 14:43:54 +0800

    Remove some redundant codes.

--- src/main/java/com/glyceryl6/staff/Main.java
+++ src/main/java/com/glyceryl6/staff/Main.java
@@ -1,13 +1,9 @@
 package com.glyceryl6.staff;
 
-import com.glyceryl6.kamikaze.registry.KEEnchantments;
 import com.glyceryl6.staff.registry.*;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.Locale;
 
@@ -15,7 +11,6 @@ import java.util.Locale;
 public class Main {
 
     public static final String MOD_ID = "staff_of_the_king_orange";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public Main(IEventBus modEventBus) {
         KOCreativeModeTabs.CREATIVE_MODE_TABS.register(modEventBus);
@@ -27,12 +22,6 @@ public class Main {
         KOMobEffects.MOB_EFFECTS.register(modEventBus);
         KOBlocks.BLOCKS.register(modEventBus);
         KOItems.ITEMS.register(modEventBus);
-        if (ModList.get().isLoaded("kamikaze_elytra")) {
-            String modId = com.glyceryl6.kamikaze.Main.MOD_ID;
-            String reg = KEEnchantments.KAMIKAZE_ELYTRA.registry().toString();
-            LOGGER.info("Mod '{}' has been successfully loaded!", modId);
-            LOGGER.info("The mod add a new enchantment named '{}'", reg);
-        }
     }
 
     public static ResourceLocation prefix(String name) {
